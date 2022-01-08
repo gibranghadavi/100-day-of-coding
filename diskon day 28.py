55cf9d3810fa0c1bb3a09e89f45d3d0c282507e0
gibranghadavi <96533091+gibranghadavi@users.noreply.github.com>
2022-01-08 18:39:37 +0700

    Add files via upload

---
+++ diskon day 28.py
@@ -0,0 +1,45 @@
+#day 28
+harga =int(input("masukkan harga :"))
+merek_hp =input("masukkan merek hp :")
+diskon=input("masukkan diskon :")
+if merek_hp == "vivo":
+    if diskon =="natal" :
+        jenis_hp == vivo_v9 and vivo_v11
+        harga_hp = harga * 5/100
+    elif diskon =="lebaran" :
+        jenis_hp == vivo_v9 and vivo_v11
+        harga_hp = harga * 10/100     
+    elif diskon =="akhirtahun" :
+        jenis_hp == vivo_v9 and vivo_v11
+        harga_hp = harga * 20/100
+    print("harga hp :",hatga_hp)
+elif merek_hp == "oppo":
+    if diskon =="natal" : 
+        jenis_hp == oppo_f1 and oppo_a5
+        harga_hp = harga * 5/100       
+    elif diskon =="lebaran"  :
+        jenis_hp == oppo_f1 and oppo_a5
+        harga_hp = harga * 10/100
+    elif diskon =="akhirtahun" :
+        jenis_hp == oppo_f1 and oppo_a5
+        harga_hp = harga * 20/100
+    print("harga hp :",hatga_hp)
+elif merek_hp == "xiomi":
+    if diskon =="natal" :
+        jenis_hp = xiomi_9 and xiomi_10
+        harga_hp = harga * 5/100
+    elif diskon =="lebaran"  :
+        jenis_hp = xiomi_9 and xiomi_10
+        harga_hp = harga * 10/100
+    elif diskon =="akhirtahun" :
+         jenis_hp = xiomi_9 and xiomi_10
+         harga_hp = harga * 20/100
+    print("harga hp :",hatga_hp)
+
+    
+        
+
+
+
+
+    
