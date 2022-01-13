9375b939ae86c2d0bfbe7cee86e8a77d5d6b0052
gibranghadavi <96533091+gibranghadavi@users.noreply.github.com>
2022-01-13 19:46:51 +0700

    Add files via upload

---
+++ matrix day 33.py
@@ -0,0 +1,9 @@
+#day 33
+
+matrix = [[4,8],
+          [3,9]]
+for x in range (0 ,len(matrix)):
+    for y in range(0 ,len(matrix[0])):
+        print (matrix[x], end= ' ' ),
+    print (matrix[y])
+        
