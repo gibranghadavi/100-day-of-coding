6748fcf89881a757e0db6c8355305eac45d73c9f
gibranghadavi <96533091+gibranghadavi@users.noreply.github.com>
2022-02-06 20:47:01 +0700

    Add files via upload

---
+++ if day 57.py
@@ -0,0 +1,14 @@
+utang=int(input("masukkan utang:"))
+if utang >=10000000:
+    bunga=10/100
+    total_utang=bunga*utang
+    potongan=bunga-utang
+elif utang <10000000:
+    bunga=5/100
+    total_utang=bunga*utang
+    potongan=bunga-utang
+else:
+    print("tidak ada bunga")
+print("total utang:",total_utang)
+print("potongan:",potongan)
+          
