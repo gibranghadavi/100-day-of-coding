defaf5cdf78449ef9987427a7c4cf97110adc577
gibranghadavi <96533091+gibranghadavi@users.noreply.github.com>
2022-02-07 21:42:47 +0800

    Add files via upload

---
+++ belajar fungsi def day58.py
@@ -0,0 +1,11 @@
+#fungsi def tanpa parameter
+def nama():
+	print ("hello, selamat malam")
+nama()
+
+#fungsi def dengan parameter
+def sapa(nama):
+	print ("hai",nama)
+	
+sapa("padli")
+sapa("rasul")
